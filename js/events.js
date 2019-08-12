a4ec8b9c47d5e0a298a99fbb15f2670a17f3b5ea
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-12 20:38:52 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -16,6 +16,15 @@ function pressIt() {
     }
   })
 }
+
+function submitIt() {
+  $('form').on('submit', function() {
+    if ($('input:first').val() === 'correct') {
+      alert('your form is going to be submitted now')
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
